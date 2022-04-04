c5a117625a2a3b267949c5005266abf9365c048b
N00bG1rl <403helen@gmail.com>
2022-04-04 17:46:05 +0300

    Clear user search

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -53,6 +53,10 @@ export const GithubProvider = ({ children }) => {
 		})
 	}
 
+	const clearUsers = () => {
+		dispatch({ type: 'CLEAR_USERS' })
+	}
+
 	const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
 	return (
@@ -61,6 +65,7 @@ export const GithubProvider = ({ children }) => {
 				users: state.users,
 				isLoading: state.isLoading,
 				searchUsers,
+				clearUsers,
 			}}
 		>
 			{children}
